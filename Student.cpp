8222bae5fcf06b7923349fa5f307ceb854fed0a3
a-ghandour <123655005+a-ghandour@users.noreply.github.com>
2024-04-22 22:28:32 +0200

    Creating Student Class

---
+++ Student.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 4/22/2024.
+//
+
+#include "Student.h"
---
+++ Student.h
@@ -0,0 +1,14 @@
+//
+// Created by HP on 4/22/2024.
+//
+
+#ifndef CS214_ASSIGNMENT_1_STUDENT_H
+#define CS214_ASSIGNMENT_1_STUDENT_H
+
+
+class Student {
+
+};
+
+
+#endif //CS214_ASSIGNMENT_1_STUDENT_H
