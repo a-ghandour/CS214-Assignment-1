c136cbae748f44541d9ac9c60b0594c2bdfe949c
Boleka3 <146965407+Boleka3@users.noreply.github.com>
2024-04-22 23:42:22 +0200

    Create InsertionSort.cpp

---
+++ Sorting Algorithms/InsertionSort.cpp
@@ -0,0 +1,13 @@
+template<typename T>
+void insertionSort(vector<T>& data){
+    int comparisons = data.size();
+    for (int i = 1; i < comparisons; ++i) {
+        T key = data[i];
+        int j = i - 1;
+        while (j >= 0 && data[j] > key) {
+            data[j + 1] = data[j];
+            j--;
+        }
+        data[j + 1] = key;
+    }
+}
