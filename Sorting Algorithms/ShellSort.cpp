1c7a3284ab1aa30c77083d651fba7ebcc00d0696
Boleka3 <146965407+Boleka3@users.noreply.github.com>
2024-04-23 18:31:27 +0200

    Create ShellSort.cpp

---
+++ Sorting Algorithms/ShellSort.cpp
@@ -0,0 +1,13 @@
+template<typename T>
+void ShellSort(vector<T>& data) {
+    int comparisons = data.size();
+    for (int space = comparisons / 2; space > 0; space = space/2) {
+        for (int i = space; i < comparisons; i++) {
+            T temp = data[i];
+            int j;
+            for ( j = i; j >= space && data[j - space] > temp; j = j-space)
+                data[j] = data[j - space];
+            data[j] = temp;
+        }
+    }
+}
