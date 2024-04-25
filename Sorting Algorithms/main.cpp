881712d8d89c6e96db6485c9af9f2fd9a4be72a3
a-ghandour <123655005+a-ghandour@users.noreply.github.com>
2024-04-25 16:40:52 +0200

    Adding main.cpp

---
+++ Sorting Algorithms/main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include "BubbleSort.cpp"
+#include "InsertionSort.cpp"
+#include "SelectionSort.cpp"
+#include "ShellSort.cpp"
+#include "Student.h"
+#include "Student.cpp"
+using namespace std;
+
+int main (){
+
+}
