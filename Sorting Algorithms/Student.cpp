27fe7caec28343a47a8bcf515785bcf29f734e85
a-ghandour <123655005+a-ghandour@users.noreply.github.com>
2024-04-25 16:27:07 +0200

    Updating Student Class

--- Sorting Algorithms/Student.cpp
+++ Sorting Algorithms/Student.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Student.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+Student::Student(std::string sid, std::string sname, double sgpa) {
+        id = sid;
+        name = sname;
+        sgpa = gpa;
+}
+
+bool Student::operator<(Student std) {
+    if(std.name < this->name){
+        return true;
+    }
+    else
+        return false;
+}
--- Sorting Algorithms/Student.h
+++ Sorting Algorithms/Student.h
@@ -4,10 +4,17 @@
 
 #ifndef CS214_ASSIGNMENT_1_STUDENT_H
 #define CS214_ASSIGNMENT_1_STUDENT_H
-
+#include <bits/stdc++.h>
+using namespace std;
 
 class Student {
-
+private:
+    string id;
+    string name;
+    double gpa;
+public:
+    Student(string sid, string sname, double sgpa);
+    bool operator< (Student std);
 };
 
 
